b39fa2a3517cc418149d4666ec49e0a559e51c56
Tom Owens <tom@towens.com>
2018-09-30 05:36:29 -0600

    How to include generic C++ code in .ino.

--- scfcontrol/scf.h
+++ scfcontrol/scf.h
@@ -1,3 +1,6 @@
 #ifndef __SCF__
 #define __SCF__
+
+extern const char * DEVTYPE = "SCF-A1";
+
 #endif
--- scfcontrol/scfcontrol.ino
+++ scfcontrol/scfcontrol.ino
@@ -1,6 +1,8 @@
 #include "scf.h"
 
 void setup() {
+    Serial.begin(115200);
+    Serial.println(DEVTYPE);
 }
 
 void loop() {
